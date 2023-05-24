116e606731bcb5a6189d4a9930151f781cd9f717
Rishabh Verma <rishabhverma@google.com>
2023-05-24 23:14:28 +0530

    feat: Add editorconfig support.

--- spanner/api/Spanner.Samples.Tests/runTests.ps1
+++ spanner/api/Spanner.Samples.Tests/runTests.ps1
@@ -15,5 +15,6 @@ import-module -DisableNameChecking ..\..\..\BuildTools.psm1
 
 Set-TestTimeout 7200
 
+dotnet format --check --fix-whitespace --fix-analyzers --verbosity diagnostic
 dotnet restore --force
 dotnet test --no-restore --test-adapter-path:. --logger:junit 2>&1 | %{ "$_" }
